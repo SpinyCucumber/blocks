fe9fec2017ebcac858d86fa38cb911c2792923fa
ethilty <spinycucumber@gmail.com>
2022-12-14 01:20:31 -0600

    Reducer test

---
+++ reducer/reduce.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@jest/globals";
+import { Mapping, Variable } from "../syntax";
+import { substitute } from "./reduce";
+
+test("should substitute variables", () => {
+    const mapping = Mapping.of([[2, Variable.forSide(0)]]);
+    const expr = Variable.forSide(2);
+    expect(substitute(expr)(mapping)).toBe(Variable.forSide(0));
+});
